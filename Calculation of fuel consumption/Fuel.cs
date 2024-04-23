2cfa724ecf051ef1414f6ba652099601403870e7
zazplay <zazplay3881@gmail.com>
2024-04-23 19:41:08 +0300

    Fuel consumption
    Added fuel consumption

---
+++ Calculation of fuel consumption/Fuel.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculation_of_fuel_consumption
+{
+    public interface Fuel
+    {
+        public double GetFuelConsumption(double km, double AvFuelCons, double costFuel)
+        {
+            return (km/AvFuelCons) * costFuel;
+        }
+    }
+}
