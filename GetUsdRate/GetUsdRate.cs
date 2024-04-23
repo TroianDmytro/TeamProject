e72e8fe3241f01b1d8449fabfd3d93426d63787b
LEGOTM <167894421+LEGOTM@users.noreply.github.com>
2024-04-23 18:59:26 +0300

    GetUsdRate
    Получение курса USD по API и конвертация UAH в USD.

---
+++ GetUsdRate/GetUsdRate.cs
@@ -0,0 +1,63 @@
+﻿namespace GetUsdRate
+{
+    public class GetUsdRate
+    {
+        private static readonly HttpClient _client;
+
+        private static async Task<decimal> ConvertToUSD(string priceUAH)
+        {
+            try
+            {
+                decimal usdRate = await GetUsdRateAPI();
+
+                if (decimal.TryParse(priceUAH, out decimal priceUAHDecimal))
+                {
+                    decimal priceUSD = priceUAHDecimal / usdRate;
+                    return priceUSD;
+                }
+                else
+                {
+                    throw new ArgumentException("Неверный формат цены в UAH");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Ошибка при конвертации цены: {e.Message}");
+                return 0;
+            }
+        }
+
+        private static async Task<decimal> GetUsdRateAPI()
+        {
+            try
+            {
+                HttpResponseMessage response = await _client.GetAsync("p24api/pubinfo?exchange&coursid=5");
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                string[] lines = responseBody.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string line in lines)
+                {
+                    if (line.Contains("USD"))
+                    {
+                        string[] parts = line.Split(':');
+                        if (parts.Length >= 5)
+                        {
+                            string rateString = parts[4].Trim().Replace(',', '.');
+                            if (decimal.TryParse(rateString, out decimal rate))
+                                return rate;
+                        }
+                    }
+                }
+                throw new Exception("Курс USD не найден в ответе API.");
+            }
+            catch (HttpRequestException e)
+            {
+                return 0;
+            }
+        }
+
+
+    }
+}
