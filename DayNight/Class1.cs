d20913d22da8f5ba2226fe864f0c6a2c5fab251e
Aleksandr16161 <sanya16sanya@ukr.net>
2024-04-23 20:07:53 +0300

    NightDay
    Смена фона

---
+++ DayNight/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace DayNight
+{
+    public class Class1
+    {
+        public static bool TimeDay()
+        {
+            if (DateTime.Now.Hour > 6 && DateTime.Now.Hour < 20)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
