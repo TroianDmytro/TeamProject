4e665f110a19fa38eeaacc7ef620cdaece5d3788
LEGOTM <167894421+LEGOTM@users.noreply.github.com>
2024-04-23 20:44:01 +0300

    Update Form1.cs

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -2,9 +2,14 @@ namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
+        private readonly HttpClient _client;
+        
         public Form1()
         {
             InitializeComponent();
+            
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri("https://api.privatbank.ua/");
         }
 
         private void Form1_Load(object sender, EventArgs e)
